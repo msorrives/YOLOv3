07bd99fafc526de9baa4e4282d1034f614ad04bd
Marta <msorrives@gmail.com>
2019-12-12 01:03:46 +0100

    Custom crop 2

--- image_segmentation.py
+++ image_segmentation.py
@@ -4,27 +4,59 @@ import glob
 
 for file in glob.glob('docs/DOTA29_Prueba/*.png'):
     img = cv2.imread(file)
+    #----- Corte 0 - 0
     x = 0
     y = 0
     h = 100
     w = 100
-    crop_img = img[y:y + h, x:x + w]
+    crop_img1 = img[y:y + h, x:x + w]
     name, extension = os.path.splitext(os.path.basename(file))
     print(name)
     print(extension)
-    filename = name + "_0_0" + extension
-    print(filename)
-    print(os.path.isfile(filename))
-    if os.path.isfile(filename):
+    filename1 = name + "_0_0" + extension
+    print(filename1)
+    print(os.path.isfile(filename1))
+    #----- Corte 0 - 1
+    x = 0
+    y = 0
+    h = 100
+    w = 100
+    crop_img2 = img[y:y + h, x:x + w]
+    filename2 = name + "_0_1" + extension
+    # ----- Corte 1 - 0
+    x = 0
+    y = 0
+    h = 100
+    w = 100
+    crop_img3 = img[y:y + h, x:x + w]
+    filename3 = name + "_1_0" + extension
+    # ----- Corte 1 - 1
+    x = 0
+    y = 0
+    h = 100
+    w = 100
+    crop_img4 = img[y:y + h, x:x + w]
+    filename4 = name + "_1_1" + extension
+    # -----
+    if os.path.isfile(filename1):
         print("File exist")
-        #os.revome(filename)
+        break
+    elif os.path.isfile(filename2):
+        print("File exist")
+        break
+    elif os.path.isfile(filename3):
+        print("File exist")
+        break
+    elif os.path.isfile(filename4):
+        print("File exist")
         break
     else:
         print("File not exist")
-        #os.chdir("docs/Prueba")
         output_directory = 'docs/Prueba'
-        imcrop = cv2.imwrite(os.path.join(output_directory, filename), crop_img)
-        #print(filename)
+        cv2.imwrite(os.path.join(output_directory, filename1), crop_img1)
+        cv2.imwrite(os.path.join(output_directory, filename2), crop_img2)
+        cv2.imwrite(os.path.join(output_directory, filename3), crop_img3)
+        cv2.imwrite(os.path.join(output_directory, filename4), crop_img4)
         print('Successfully saved')
 
 
@@ -33,67 +65,3 @@ for file in glob.glob('docs/DOTA29_Prueba/*.png'):
 
 
 
-
-
-#----------------------------------------------
-#for filename in glob.glob('docs/DOTA29_Prueba/*.png'): #assuming gif
-#    print(filename)
-#    img = cv2.imread(filename)
-#    os.chdir("docs/Prueba")
-#    x = 0
-#    y = 0
-#    h = 100
-#    w = 100
-#    crop_img = img[y:y + h, x:x + w]
-#    filename = 'savedImage.png'
-#    cv2.imwrite(filename, crop_img)
-
-#print('Successfully saved')
-#----------------------------------------------
-#import cv2
-#import os
-
-## Using cv2.imread() method
-## to read the image
-#img = cv2.imread("docs/kite.jpg")
-#print("imread")
-
-## Change the current directory
-## to specified directory
-#os.chdir("docs/Prueba")
-
-#x= 0
-#y= 100
-#h= 1000
-#w= 1000
-#crop_img = img[y:y+h, x:x+w]
-
-## Filename
-#filename = 'savedImage3.jpg'
-
-
-## Using cv2.imwrite() method
-## Saving the image
-#cv2.imwrite(filename, crop_img)
-#print('Successfully saved')
-#----------------------------------------------
-#def segmentize (image_path, segment_width=200, segment_height=50):
-#    # Croping Formula ==> y:h, x:w
-#    idx, x_axis, x_width,  = 1, 0, segment_width
-#    y_axis, y_height = 0, segment_height
-#    img = cv2.imread(image_path)
-#    height, width, dept = img.shape
-#    print("I'm a dog!")
-#    while y_axis <= height:
-#        while x_axis <= width:
-#            crop = img[y_axis:y_height, x_axis:x_width]
-#            x_axis=x_width
-#            x_width+=segment_width
-#            cropped_image_path = "crop/crop%d.png" % idx
-#            print("I'm a cat!")
-#            cv2.imwrite(cropped_image_path, crop)
-#            idx+=1
-#        y_axis += segment_height
-#        y_height += segment_height
-#        x_axis, x_width = 0, segment_width
-#    img.show()
