2bf0ddf33b722cfef7778d9814f54eb6d90de784
Marta <msorrives@gmail.com>
2020-01-07 20:29:03 +0100

    Config version 1

--- core/config.py
+++ core/config.py
@@ -24,7 +24,8 @@ __C.YOLO                      = edict()
 
 
 # Set the class name "./data/classes/coco.names"
-__C.YOLO.CLASSES              = "./data/classes/yymnist.names"
+#__C.YOLO.CLASSES              = "./data/classes/yymnist.names"
+__C.YOLO.CLASSES              = "./data/classes/dota.names"
 __C.YOLO.ANCHORS              = "./data/anchors/basline_anchors.txt"
 __C.YOLO.STRIDES              = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE     = 3
@@ -33,7 +34,8 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 # Train options
 __C.TRAIN                     = edict()
 
-__C.TRAIN.ANNOT_PATH          = "./data/dataset/yymnist_train.txt"
+#__C.TRAIN.ANNOT_PATH          = "./data/dataset/yymnist_train.txt"
+__C.TRAIN.ANNOT_PATH          = "./data/dataset/dota29_train.txt"
 __C.TRAIN.BATCH_SIZE          = 4
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = [416]
@@ -48,7 +50,8 @@ __C.TRAIN.EPOCHS              = 30
 # TEST options
 __C.TEST                      = edict()
 
-__C.TEST.ANNOT_PATH           = "./data/dataset/yymnist_test.txt"
+#__C.TEST.ANNOT_PATH           = "./data/dataset/yymnist_test.txt"
+__C.TEST.ANNOT_PATH           = "./data/dataset/dota29_test.txt"
 __C.TEST.BATCH_SIZE           = 2
 __C.TEST.INPUT_SIZE           = 544
 __C.TEST.DATA_AUG             = False
