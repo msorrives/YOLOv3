c0537971b03e8124d9c012d8eef51eb2e50079d5
Marta <msorrives@gmail.com>
2020-01-21 01:09:08 +0100

    Train version 1

--- train.py
+++ train.py
@@ -1,16 +1,3 @@
-#! /usr/bin/env python
-# coding=utf-8
-#================================================================
-#   Copyright (C) 2019 * Ltd. All rights reserved.
-#
-#   Editor      : VIM
-#   File name   : train.py
-#   Author      : YunYang1994
-#   Created date: 2019-07-18 09:18:54
-#   Description :
-#
-#================================================================
-
 import os
 import time
 import shutil
@@ -23,7 +10,7 @@ from core.yolov3 import YOLOv3, decode, compute_loss
 from core.config import cfg
 
 #trainset = Dataset('train')
-trainset = Dataset('./root/datatmp/maguilera/DOTA-0_29/train/images/')
+trainset = Dataset('train')
 logdir = "./data/log"
 steps_per_epoch = len(trainset)
 global_steps = tf.Variable(1, trainable=False, dtype=tf.int64)
